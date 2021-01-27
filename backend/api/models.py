e3a91ba1d108fc3f54ae02da93d74dacb6a053ec
Sam Ball <stmball@gmail.com>
2021-01-27 18:13:24 +0000

    Adding backend and test frontend to application
    To run:
    
    - Enter the backend folder and do the following:
        - `pipenv shell` to enter the virtual environment.
        - `pipenv install` to install dependencies
        - `python manage.py createsuperuser` to create a superuser (the
          sqlite database is in the `.gitignore` file so this will need to
          be done each time. Good security practice!
        - `python manage.py runserver` to start the API server on
          `localhost:8000`
    - Then enter the `frontend` folder and do the following:
        - `npm install` to install dependencies
        - `npm run start` to start the node server on `localhost:3000`.
    
    The node server is running a react app, that when the test button is
    pressed should communicate with the Django API app to fetch the data
    using the django rest api library.

---
+++ backend/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Test model to check the API works
+class Ping(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
