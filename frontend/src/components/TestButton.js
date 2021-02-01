5cb4e77d9831c88482b0bb3ae5b746f9ec86bc3c
rob.treharne <rob.treharne@gmail.com>
2021-02-01 21:22:51 +0000

    models added and admin updated

--- frontend/src/components/TestButton.js
+++ frontend/src/components/TestButton.js
@@ -3,7 +3,7 @@ function TestButton(props) {
     function pingTest(e) { 
         e.preventDefault();
         // We fetch the pings API
-        fetch("http://localhost:8000/api/pings")
+        fetch("http://127.0.0.1:8000/api/pings")
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
