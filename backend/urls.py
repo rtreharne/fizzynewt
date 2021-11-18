0e47c6e92521fda66bcde36e502d36ecfacfdaba
Robert Treharne <R.Treharne@liverpool.ac.uk>
2021-11-18 13:50:40 +0000

    migrations issue fix

--- backend/urls.py
+++ backend/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 router.register(r"pings", views.PingView, 'ping')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('auth/', include('authentication.urls'))
 ]
