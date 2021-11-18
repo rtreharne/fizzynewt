38382290374023bca36c277ef3198d1b84440be3
Robert Treharne <R.Treharne@liverpool.ac.uk>
2021-11-18 14:11:54 +0000

    fixed?

--- backend/settings.py
+++ backend/settings.py
@@ -42,7 +42,7 @@ AUTH_USER_MODEL = 'authentication.User'
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
