fb97160a6e7d8f59b2488e84985eb1a01dc1cdef
Robert Treharne <R.Treharne@liverpool.ac.uk>
2021-11-29 11:19:34 +0000

    unique course by institute tested

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Course
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -7,3 +8,15 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ["id", "code", "name"]
+
+    def validate(self, attrs):
+        user = self.context.get('request', None).user
+        institution = user.institution
+        code = attrs.get('code', None)
+
+        try:
+            obj = Course.objects.get(code=code, institution=institution)
+            raise serializers.ValidationError('This course already exists at {0}'.format(institution.name))
+        except ObjectDoesNotExist:
+            return attrs
+
